7a74ccd7b691579f9e6e51f4120e79814f9a7f66
tunde <icepaigez@gmail.com>
2023-05-27 19:51:53 +0100

    fixed the function to extract job openings

--- src/controllers/v0/jobs/routes/job_search.router.js
+++ src/controllers/v0/jobs/routes/job_search.router.js
@@ -13,16 +13,20 @@ router.post('/', async (req, res) => {
 	try {
 		if (Object.keys(req.body).length != 0) {
 			let resume = req.body.resume;
-			let role = req.body.role
+			let role = req.body.role;
+
 			if (role.length > 0) {
-				let job_urls = await getJobLinks(role);
-				res.status(200).send({ "url":job_urls });
+				let data = await getJobLinks(role);
+				let urls = data.map(obj => obj.value)
+				if (urls) {
+					res.status(200).send({ 'urls':urls })
+				}
 			} else {
-				res.status(500).send({ 'error': 'something blew up' })
+				res.status(500).send({ 'error': 'empty role entered' })
 			}
 		}
 	} catch(err) {
-		console.err('something went wrong', err)
+		console.error('something went wrong', err)
 		next(err)
 	}
 });
