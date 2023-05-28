8305338384f2b188a70cd1349412faf68e0fd0ee
tunde <icepaigez@gmail.com>
2023-05-28 19:29:29 +0100

    code fix

--- src/controllers/v0/jobs/routes/job_search.router.js
+++ src/controllers/v0/jobs/routes/job_search.router.js
@@ -12,17 +12,17 @@ const { get_job_prep } = require('./gpt');
 router.post('/', async (req, res) => {
 	try {
 		if (Object.keys(req.body).length != 0) {
-			let resume = req.body.resume;
+			let resume = JSON.stringify(req.body.resume);
 			let role = req.body.role;
 
 			if (role.length > 0) {
 				let url_array = await getJobLinks(role);
 				if (url_array) {
+					console.log('from the router', url_array)
 					let job_desc_array = await getJobData(url_array);
 					if (job_desc_array) {
 						let resume_array = await get_job_prep(job_desc_array, resume, role)
 						if (resume_array) {
-							console.log('from the router', resume_array)
 							res.status(200).send({ 'resumes':resume_array })
 						}
 					}
